1da96f5d53d131855b8d2ca4664974ae736025de
Niamh Gowran <91330763+ngowran@users.noreply.github.com>
2022-06-25 23:30:14 +0000

    Render React from Django.

--- timelogger/settings.py
+++ timelogger/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2*0hh!k#83j+x&kxs#*lok5_z3sx*)ufhb6_7hjc%a30tqzc7&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["78716026-a6f7-40e2-8a2b-62be94f839c4.id.repl.co",]
 
 
 # Application definition
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'timelogger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+          os.path.join(BASE_DIR, "timelogger/frontend/build")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, "timelogger/frontend/build/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

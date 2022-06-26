2410572c8c31c071ea17af4e0857dc12caa17d20
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-06-26 15:58:12 +0100

    Update App.js
    favicon setup

--- timelogger/frontend/src/App.js
+++ timelogger/frontend/src/App.js
@@ -1,7 +1,14 @@
 import logo from './assets/logo.png';
 import './App.css';
+import icon from './assets/favicon.ico';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const favicon = document.getElementById('favicon');
+    favicon.setAttribute('href', icon);
+}, []);
+
   return (
     <div className="App">
       <header className="App-header">

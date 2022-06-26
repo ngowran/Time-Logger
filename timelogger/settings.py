bab45ea6a99d83878da5ad6208ffa5215a3566bb
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-06-26 15:57:37 +0100

    Update settings.py
    db setup

--- timelogger/settings.py
+++ timelogger/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'timelogger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
     }
 }
 
